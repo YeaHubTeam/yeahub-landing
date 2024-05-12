713b14b382cb7a2a6b86b5127c1416595f47576f
Buffik <Som16@mail.ru>
2024-05-12 23:01:14 +0300

    YH-119: replace RegistrationLabel to widgets

--- src/features/registration/index.ts
+++
@@ -1,1 +0,0 @@
-export { RegistrationLabel } from './ui/RegistrationLabel';
--- src/widgets/authentication/login/ui/LoginCreateForm/LoginCreateForm.tsx
+++ src/widgets/authentication/login/ui/LoginCreateForm/LoginCreateForm.tsx
@@ -3,10 +3,10 @@ import { FormProvider, useForm } from 'react-hook-form';
 
 import { LoginForm } from '@/features/authentication';
 import { SocialRegister } from '@/features/authentication/ui/SocialRegister/SocialRegister';
-import { RegistrationLabel } from '@/features/registration';
 
 import { loginSchema } from '../../model/lib/validation/loginSchema';
 import { LoginSchema } from '../../model/types/loginTypes';
+import { RegistrationLabel } from '../RegistrationLabel/RegistrationLabel';
 
 export const LoginCreateForm = () => {
 	const methods = useForm<LoginSchema>({
