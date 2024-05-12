741602ae9ac37c44f2461992fd5f8e50e45dba75
Vladimir <62429354+Buffik@users.noreply.github.com>
2024-05-13 00:01:01 +0300

    YH-119: Верстка страницы авторизации (#14)
    * Refactor how to join section (#12)
    
    * refactor: carousel styles
    
    * refactor: decompose HowToJoinBlock, add smooth scroll
    
    * fix: name of background color prop
    
    * refactor: names of interfaces, mapping of steps, add descriptions to images at steps data
    
    * YH-119: make fullsrceen login page layout
    
    * YH-119: add login form
    
    * YH-119: add submit button
    
    * YH-119: add alternative login block
    
    * YH-119: add registration label
    
    * YH-119: fix from comments
    
    * YH-119: replace RegistrationLabel to widgets
    
    * YH-119: fix from comments

---
+++ src/features/authentication/login/ui/RegistrationLabel/RegistrationLabel.tsx
@@ -0,0 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'yeahub-ui-kit';
+
+import styles from './RegistrationLabel.module.css';
+
+export const RegistrationLabel = () => {
+	const navigate = useNavigate();
+	return (
+		<div className={styles.wrapper}>
+			<p>Нет аккаунта?</p>
+			<Button
+				tagName="a"
+				theme="link"
+				value={'Зарегистрироваться как специалист'}
+				onClick={() => navigate('/')}
+			/>
+		</div>
+	);
+};
--- src/pages/LoginPage/ui/LoginPage.tsx
+++ src/pages/LoginPage/ui/LoginPage.tsx
@@ -1,11 +1,15 @@
-import { AuthBlock } from '@/widgets/AuthBlock';
+import { LoginCreateForm } from '@/widgets/authentication/login';
 import { AuthLayout } from '@/widgets/AuthLayout';
-//import styles from './LoginPage.module.css';
+
+import styles from './LoginPage.module.css';
 
 export const LoginPage = () => {
 	return (
 		<AuthLayout>
-			<AuthBlock />
+			<div className={styles.wrapper}>
+				<h1 className={styles.title}>Вход в личный кабинет</h1>
+				<LoginCreateForm />
+			</div>
 		</AuthLayout>
 	);
 };
