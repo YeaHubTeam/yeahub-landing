3243a4f2323a6ced5fa7db34a275cb906dfab0c2
Vladimir <62429354+Buffik@users.noreply.github.com>
2024-06-04 22:17:11 +0300

    YH-148: remove functions from JSX

--- src/features/authentication/login/ui/RegistrationLabel/RegistrationLabel.tsx
+++ src/features/authentication/login/ui/RegistrationLabel/RegistrationLabel.tsx
@@ -5,10 +5,13 @@ import styles from './RegistrationLabel.module.css';
 
 export const RegistrationLabel = () => {
 	const navigate = useNavigate();
+	const handleClickNavigation = () => {
+		navigate('/registration');
+	};
 	return (
 		<div className={styles.wrapper}>
 			<p>Нет аккаунта?</p>
-			<Button tagName="a" theme="link" onClick={() => navigate('/registration')}>
+			<Button tagName="a" theme="link" onClick={handleClickNavigation}>
 				Зарегистрироваться как специалист
 			</Button>
 		</div>
--- src/features/registration/ui/LoginLabel/LoginLabel.tsx
+++ src/features/registration/ui/LoginLabel/LoginLabel.tsx
@@ -5,10 +5,13 @@ import styles from './LoginLabel.module.css';
 
 export const LoginLabel = () => {
 	const navigate = useNavigate();
+	const handleClickNavigation = () => {
+		navigate('/registration');
+	};
 	return (
 		<div className={styles.wrapper}>
 			<p>Уже есть аккаунт?</p>
-			<Button tagName="a" theme="link" onClick={() => navigate('/login')}>
+			<Button tagName="a" theme="link" onClick={handleClickNavigation}>
 				Войти
 			</Button>
 		</div>
