f90e19d2a75eaeeaccff1bbb32d479209270ae4a
“Elena <bachi.elena@gmail.com>
2024-06-10 21:05:08 +0200

    Исправлен блок с преимуществами: добавлен заголовок, заменены иконки

--- src/widgets/InterviewBlock/ui/AdvantagesList/AdvantagesList.tsx
+++ src/widgets/InterviewBlock/ui/AdvantagesList/AdvantagesList.tsx
@@ -1,5 +1,6 @@
-import events from '@/shared/assets/images/eventsImg.png';
-import eventsChecked from '@/shared/assets/images/eventsImgChecked.png';
+import { KnowledgebaseIcon } from '@/shared/ui/KnowledgebaseIcon';
+import { ProgressIcon } from '@/shared/ui/ProgressIcon';
+import { TrainingIcon } from '@/shared/ui/TrainingIcon';
 
 import cls from './AdvantagesList.module.css';
 
@@ -8,17 +9,17 @@ export const AdvantagesList = () => {
 		<div className={cls.advantages}>
 			<ul className={cls['advantages-list']}>
 				<li>
-					<img src={events} alt="Большая база вопросов" />
+					<KnowledgebaseIcon />
 					<h3>Большая база вопросов</h3>
 					<p>Изучай технологии по актуальным теоритеческим вопросам</p>
 				</li>
 				<li>
-					<img src={eventsChecked} alt="Прогресс обучения" />
+					<ProgressIcon />
 					<h3>Прогресс обучения</h3>
 					<p>Смотрите аналитику по пройденным темам, повторяйте изученные</p>
 				</li>
 				<li>
-					<img src={events} alt="Удобный тренажёр" />
+					<TrainingIcon />
 					<h3>Удобный тренажёр</h3>
 					<p>Улучшайте свои навыки в нашем тренажёре по запоминанию вопросов</p>
 				</li>
